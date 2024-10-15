7d3baf11f5aab1e688174c55c12efa963f21b4c1
Michal Fularz <michal.fularz@gmail.com>
2024-10-15 10:18:02 +0200

    Added ex 1

---
+++ wdsi/lab_01/lab_01_ex_1.py
@@ -0,0 +1,82 @@
+import queue
+
+
+def create_graph() -> dict[int, list[int]]:
+    graph = {
+        1: [2, 3],
+        2: [1, 5],
+        3: [1, 4],
+        4: [3, 6],
+        5: [2, 6, 8],
+        6: [4, 5, 6, 8],
+        7: [6],
+        8: [5, 6],
+    }
+
+    return graph
+
+
+def bfs(s, g, graph: dict):
+    # s - wierzchołek startowy
+    # g - wierzchołek docelowy
+    # nodes - lista wierzchołków
+    nodes = list(graph.keys())
+
+    # lista odwiedzonych wierzchołków
+    visited = set()
+    # słownik poprzedników
+    parent = {n: None for n in nodes}
+
+    q = queue.Queue()
+
+    # dodaj wierzchołek startowy
+    # push(q, s)
+    q.put(s)
+    # ustaw jego poprzednika jako jego samego, aby oznaczyć go jako odwiedzony
+    parent[s] = s
+    # dopóki kolejka nie jest pusta, czyli są jeszcze jakieś wierzchołki do odwiedzenia
+    # while not empty(q):
+    while not q.empty():
+        # pobierz następny wierzchołek i usuń go z kolejki
+        # cur_n = front(q)
+        # pop(q)
+        cur_n = q.get()
+
+        # przerwij jeśli dotarliśmy do celu
+        if cur_n == g:
+            break
+
+        # dla wszystkich krawędzi z aktualnego wierzchołka
+        # for nh in edges(cur_n):
+        for nh in graph[cur_n]:
+            # jeśli sąsiad nie był jeszcze odwiedzony
+            if nh not in visited:
+                # oznacz jako odwiedzony i dodaj do kolejki
+                parent[nh] = cur_n
+                # add(visited, nh)
+                visited.add(nh)
+                # push(q, nh)
+                q.put(nh)
+
+    # ścieżka do wierzchołka docelowego
+    path = []
+
+    # zaczynamy od wierzchołka docelowego i cofamy się po znalezionej ścieżce
+    cur_n = g
+    # dopóki nie dotrzemy do startu
+    while cur_n != s:
+        # dodajemy aktualny wierzchołek i przechodzimy do poprzednika
+        # add(path, cur_n)
+        path.append(cur_n)
+        cur_n = parent[cur_n]
+    # wierzchołki są w odwrotnej kolejności, więc odwracamy listę
+    # reverse(path)
+    path = list(reversed(path))
+
+    return path
+
+
+def ex_1():
+    graph = create_graph()
+    path = bfs(1, 4, graph)
+    print(path)
---
+++ wdsi/lab_01_main.py
@@ -0,0 +1,9 @@
+from lab_01.lab_01_ex_1 import ex_1
+
+
+def main():
+    ex_1()
+
+
+if __name__ == '__main__':
+    main()
